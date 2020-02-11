210bdb2b5ed22b046c9958a2dbe604fc9d5e68d6
mgalliou <matthias.galliou@gmail.com>
2020-02-11 18:19:27 +0100

    fix base_is_valid in atoi_base

--- src/ft_atoi_base.s
+++ src/ft_atoi_base.s
@@ -6,26 +6,48 @@ section .text
 	extern _ft_strchr
 	extern _ft_isspace
 
+ft_issign:
+	push rbp
+	mov  rbp, rsp
+	sub  rsp, 16
+
+	xor rax, rax
+	cmp rdi, '+'
+	je  .true
+	cmp rdi, '-'
+	je  .true
+	ja  .exit
+
+.true:
+	mov rax, 1
+
+.exit:
+	leave
+	ret
+
 base_is_valid:
 	push rbp
 	mov  rbp, rsp
 	sub  rsp, 16
 
-	push rdi
-	cmp  rdi, 0 
+	cmp  rdi, 0
 	je   .false     ; jmp if base str ptr is null
+	push rdi
 	call _ft_strlen
+	pop  rdi
 	cmp  al, 2      ; jmp if base len is less than 2
 	jle  .false
-	pop rdi
 
 .loop:
-	cmp   byte [rdi], '+'  
+	cmp   byte [rdi], '+'
 	je    .false            ; jmp if char is + sign
 	cmp   byte [rdi], '-'
 	je    .false            ; jmp if char is - sign
-	movzx rsi, byte [rdi + 1]
+	movzx rsi, byte [rdi]
+	push  rdi
+	inc   rdi
 	call  _ft_strchr
+	pop   rdi
 	cmp   rax, 0			; check for duplicate char in str
 	jne   .false
 	cmp   byte [rdi + 1], 0
@@ -34,6 +56,7 @@ base_is_valid:
 	jmp   .loop
 
 .true:
+	mov rax, 1
 	jmp .exit
 
 .false:
@@ -48,17 +71,47 @@ _ft_atoi_base:
 	push rbp
 	mov  rbp, rsp
 	sub  rsp, 16
+	push rbx
 
 	push rdi           ; push number string
 	mov  rdi, rsi
 	call base_is_valid
+	pop  rdi           ; pop number string
 	cmp  rax, 0
 	je   .done         ; jmp if base is not valid
-	pop  rdi           ; pop number string
+	mov  rax, 5		   ; tmp return value to check base_is_valid is working
+	jmp  .done
 
-
-
+;	mov rbx, 1
+;.skip_space_loop:
+;	push  rdi
+;	movzx rdi, byte [rdi]
+;	call  _ft_isspace
+;	cmp   rax, 0
+;	je   .skip_sign_loop
+;	pop   rdi
+;	inc   rdi
+;	jmp   .skip_space_loop
+;
+;.skip_sign_loop:
+;	push  rdi
+;	movzx rdi, byte [rdi]
+;	call  ft_issign
+;	cmp   rax, 0
+;	je    .convert   ;jmp if not a sign
+;	pop   rdi
+;	cmp   byte [rdi], '-'
+;	je    .negate
+;.after_negate:
+;	inc  rdi
+;	jmp .skip_sign_loop
+;
+;.negate:
+;	neg rbx
+;
+;.convert:
 
 .done:
+	pop rbx
 	leave
 	ret
