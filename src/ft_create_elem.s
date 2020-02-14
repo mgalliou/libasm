cbc0f21a5e485aeadb3eea96e67694b22cf0e2e4
mgalliou <matthias.galliou@gmail.com>
2020-02-14 10:55:20 +0100

    comments ft_create_elem and use t_list struc macro instead of harcoded values

--- src/ft_create_elem.s
+++ src/ft_create_elem.s
@@ -12,18 +12,19 @@ _ft_create_elem:
 	sub  rsp, 8
 	push rbx
 
-	mov  rbx, rdi
-	mov  rdi, t_list_size
+	mov  rbx, rdi         ; save ptr on data 
+	mov  rdi, t_list_size ; prepare malloc call
 	call _malloc
 	cmp  rax, 0
-	je   .failed            ;jmp if malloc failed
-	mov  rdi, rbx
-	mov  qword [rax], rdi
-	mov  qword [rax + 8], 0
+	je   .failed          ; jmp if malloc failed
+
+	mov  qword [rax + t_list.data], rbx ; make new_elem->data point on data
+	mov  qword [rax + t_list.next], 0   ; make new_elem->next point on NULL
 	jmp  .done
 	
 .failed:
 	xor  rax, rax
+
 .done:
 	pop  rbx
 	leave
