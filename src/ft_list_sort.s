3ed3748bbe648a8233beb57343385f24f1ce4645
mgalliou <matthias.galliou@gmail.com>
2020-02-14 14:03:36 +0100

    finish implemanting ft_list_sort

--- src/ft_list_sort.s
+++ src/ft_list_sort.s
@@ -10,9 +10,9 @@ _ft_list_sort:
 	mov  rbp, rsp
 	sub  rsp, 8
 	push rbx
-	;push r12
-	;push r13
-	;push r14
+	push r12
+	push r13
+	push r14
 
 	cmp rsi, 0
 	je  .done       ; jmp if cmp is null
@@ -21,37 +21,34 @@ _ft_list_sort:
 	mov rbx, [rdi]  ; save list beg ptr in tmp reg
 	cmp rbx, 0
 	je  .done       ; jmp if beg list ptr is null
-	;mov r14, rbx    ; save list beg ptr
-	;mov r12, rsi    ; save ptr on cmp
+	mov r12, rsi    ; save ptr on cmp
+	mov r13, rbx    ; save list beg ptr
 
-;.loop:
+.loop:
 	cmp qword [rbx + t_list.next], 0
 	je  .done ; jmp if cur->next is null
 
-	;mov  rdi, [rbx + t_list.data]
-	;mov  rsi, [rcx + t_list.data]
-	;push rsi
-	;call r12
-	;pop  rsi
-	;cmp  rax, 0
-	;jne  .swap_data
-	;mov  rbx, [rbx + t_list.next] ; mov next ptr to cur ptr
-	;jmp  .loop
+	mov  r14, [rbx + t_list.next] ; save ptr on next elem
+	mov  rdi, [rbx + t_list.data]
+	mov  rsi, [r14 + t_list.data]
+	call r12
+	cmp  rax, 0
+	jg  .swap_data
+	mov  rbx, [rbx + t_list.next] ; mov next ptr to cur ptr
+	jmp  .loop
 
 .swap_data:
-	mov rax, [rbx + t_list.next] ; save ptr on next elem
 	mov rcx, [rbx + t_list.data] ; save ptr on cur elem data
-	mov rdi, [rax + t_list.data] ; save ptr on next elem data
+	mov rdi, [r14 + t_list.data] ; save ptr on next elem data
 	mov [rbx + t_list.data], rdi ; move next elem data to cur data
-	mov [rax + t_list.data], rcx ; move cur data to next elem data 
-	;mov rbx, r14 ; mov cur ptr to beg list ptr
-	;jmp .loop
-
+	mov [r14 + t_list.data], rcx ; move cur data to next elem data 
+	mov rbx, r13                 ; move cur ptr to beg list ptr
+	jmp .loop
 
 .done:
-	;pop r14
-	;pop r13
-	;pop r12
+	pop r14
+	pop r13
+	pop r12
 	pop rbx
 	leave
 	ret
--- test/src/suite_ft_list_sort.c
+++ test/src/suite_ft_list_sort.c
@@ -6,12 +6,32 @@
 /*   By: mgalliou <mgalliou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/14 11:42:29 by mgalliou          #+#    #+#             */
-/*   Updated: 2020/02/14 13:40:56 by mgalliou         ###   ########.fr       */
+/*   Updated: 2020/02/14 14:03:16 by mgalliou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tests.h"
 #include "string.h"
+
+static TEST(when_four_mixed_elem)
+{
+	t_list *lst;
+
+	lst = ft_create_elem("A");
+	ft_list_push_front(&lst, "D");
+	ft_list_push_front(&lst, "B");
+	ft_list_push_front(&lst, "C");
+	ft_list_sort(&lst, strcmp);
+	assert_not_null(lst);
+	assert_str_equal("A", lst->data);
+	lst = lst->next;
+	assert_str_equal("B", lst->data);
+	lst = lst->next;
+	assert_str_equal("C", lst->data);
+	lst = lst->next;
+	assert_str_equal("D", lst->data);
+	assert_null(lst->next);
+}
 
 static TEST(when_tree_elem_in_reverse_order)
 {
@@ -71,5 +91,6 @@ TEST_SUITE(suite_ft_list_sort)
 	RUN_TEST(when_cmp_function_is_null);
 	RUN_TEST(when_two_elem_in_reverse_order);
 	RUN_TEST(when_tree_elem_in_reverse_order);
+	RUN_TEST(when_four_mixed_elem);
 }
 
