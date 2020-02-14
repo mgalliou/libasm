a7c0f9725163bb9968ad29e8ae15a8ab85b6f765
mgalliou <matthias.galliou@gmail.com>
2020-02-14 10:57:30 +0100

    improve ft_list_push_front comments

--- src/ft_list_push_front.s
+++ src/ft_list_push_front.s
@@ -11,15 +11,16 @@ _ft_list_push_front:
 	mov  rbp, rsp
 	sub  rsp, 8
 	push rbx
-	
-	mov  rbx, rdi        ; save ptr on list addr
-	mov  rdi, rsi        ; move data to first param reg for create_elem
+
+	mov  rbx, rdi        ; save ptr on list beg addr
+	mov  rdi, rsi        ; prepare ft_create_elem call with data ptr
 	call _ft_create_elem
 	cmp  rax, 0
-	je   .done           ; jmp if create_elem failed
-	mov  rdi, [rbx]      ; 
-	mov  [rax + t_list.next], rdi  ; make new_elem->next point on list beg
-	mov  [rbx], rax      ; make beg list ptr point on new elem
+	je   .done           ; jmp if ft_create_elem failed
+
+	mov  rdi, [rbx]
+	mov  [rax + t_list.next], rdi ; make new_elem->next point on list beg
+	mov  [rbx], rax               ; make list beg ptr point on new elem
 
 .done:
 	pop rbx
