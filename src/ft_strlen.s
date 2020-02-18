27422daef0d0383bd9aab72731d19c1aad63d439
mgalliou <matthias.galliou@gmail.com>
2020-02-18 18:30:52 +0100

    optimize ft_strlen.s using string instruction

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -6,17 +6,15 @@ section .text
 _ft_strlen:
 	push rbp
 	mov  rbp, rsp
-	sub  rsp, 16
 
-	mov rax, rdi ; save string beg ptr
-.loop:
-	cmp byte [rdi], ''
-	jz  .done ; if byte is zeroed jump out of the loop
-	inc rdi   ; else increment ptr addr
-	jmp .loop
+	mov   rdx, rdi  ; save string beg ptr
+	cld             ; clear direction flag
+	xor   al, al    ; set byte to be scanned by scas at 0
+	mov   rcx, -1   ; start decremanting at -1
+	repne scasb     ; rep until byte scanned (al) is not found
 
-.done:
-	sub rdi, rax ; sub end addr to beg addr
+	dec rdi      ; scasb increment rdi one more time after al has been found
+	sub rdi, rdx ; sub end addr to beg addr
 	mov rax, rdi ; move diff to ret register
 
 	leave
