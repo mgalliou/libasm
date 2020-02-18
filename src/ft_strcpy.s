ca2d221f77fa62b4f5a1a5e4ae7e1a8215b08d1f
mgalliou <matthias.galliou@gmail.com>
2020-02-18 18:48:39 +0100

    optimize ft_strcpy using string insctruction

--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -6,16 +6,13 @@ section .text
 _ft_strcpy:
 	push rbp
 	mov  rbp, rsp
-	sub  rsp, 16
-
-	xor rcx, rcx ; zero rcx
-.loop:
-	movzx rax, byte [rsi + rcx] ; put in rcx byte to be copied
-	mov   byte [rdi + rcx], al  ; cpy byte to dst
-	inc   rcx
-	test  al, al ; if copied byte is not zero continue
-	jne   .loop
 
 	mov rax, rdi
+	cld
+.loop:
+	movsb
+	cmp byte [rdi - 1], 0
+	loopne .loop
+
 	leave
 	ret
